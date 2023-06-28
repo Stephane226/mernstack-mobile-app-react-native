3cba9b426ea60402030215c5eb227e145081445e
Masaaki Stephane <62558026+Stephane226@users.noreply.github.com>
2023-06-28 13:59:19 +0300

    models and routes

--- project/backend/routes/products.js
+++ project/backend/routes/products.js
@@ -1,48 +1,31 @@
-const express = require('express')
-const router = express.Router()
-const Product = require('./models/product')
-
-
-
-router.get('/', (req, res) =>{
-    res.send('hello api')
-})
-
-
-
-
-router.get(`/`, (req, res) =>{
-   console.log(api)
-    const porduct = {
-        id: 1,
-        name: 'masaaki stephane',
-        image: 'https://mc-69e30ef4-758e-4371-ac6f-2657-cdn-endpoint.azureedge.net/-/media/IMA/icons/IMA-Logo-Social-Sharing.ashx?rev=c0e5e2be67f84f4081d61abdb80622e0'
-    }
-    res.send(porduct)
-})
+const {Product} = require('../models/product');
+const express = require('express');
+const router = express.Router();
 
 router.get(`/`, async (req, res) =>{
+    const productList = await Product.find();
 
-     const products = await Product.find()
-     res.send(products)
- })
-
- 
+    if(!productList) {
+        res.status(500).json({success: false})
+    } 
+    res.send(productList);
+})
 
 router.post(`/`, (req, res) =>{
-     const product = new Product({
-        name :req.body.name,
-        image : req.body.image,
-        countInStock : req.body.countInStock
-     })
-     product.save().then((createdProduct) =>{
-        res.status(201).json(createdProduct)
-     }).catch((err)=>{
-        res.status(500).json({
-            error:err,
-            success:false,
-        })
-     })
- })
+    const product = new Product({
+        name: req.body.name,
+        image: req.body.image,
+        countInStock: req.body.countInStock
+    })
 
- module.exports = router
+    product.save().then((createdProduct=> {
+        res.status(201).json(createdProduct)
+    })).catch((err)=>{
+        res.status(500).json({
+            error: err,
+            success: false
+        })
+    })
+})
+
+module.exports =router;
