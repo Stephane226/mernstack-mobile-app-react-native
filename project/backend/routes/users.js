2d71afaf5919cd025823d32065142223ed0a5199
Masaaki Stephane <62558026+Stephane226@users.noreply.github.com>
2023-06-29 11:49:29 +0300

    user login checks

--- project/backend/routes/users.js
+++ project/backend/routes/users.js
@@ -1,6 +1,8 @@
 const {User} = require('../models/user');
 const express = require('express');
 const router = express.Router();
+const bcrypt = require('bcrypt')
+
 
 router.get(`/`, async (req, res) =>{
     const userList = await User.find();
@@ -17,7 +19,7 @@ router.post('/' , async (req,res) =>{
     let user = new User({
         name: req.body.name,
         email: req.body.email,
-        passwordHash: req.body.passwordHash,
+        passwordHash: bcrypt.hashSync(req.body.password, 10),
         phone: req.body.phone,
         isAdmin: req.body.isAdmin,
         street: req.body.street,
@@ -37,4 +39,51 @@ router.post('/' , async (req,res) =>{
 
 })
 
+
+
+//get list of users
+
+router.get(`/`, async (req, res) =>{
+    const users = await User.find().select("-passwordHash");
+
+    if(!users) {
+        res.status(500).json({success: false, message : "no user found"})
+    } 
+    res.send(users);
+})
+
+
+//find a user by id
+
+router.get('/:id', async  (req, res) => {
+    const user = await User.findById(req.params.id).select("name phone email");
+
+    if(!user){
+        return res.status(404).send('the category cannot be created...')
+    }
+
+    res.send(user);
+    
+})
+
+
+//user login checks
+
+router.post('/login', async  (req, res) => {
+    const user = await User.findOne({email : req.body.email})
+
+    if(!user){
+        return res.status(404).send('user not exist...')
+    }
+
+    if(user && bcrypt.compareSync( req.body.password , user.passwordHash)){
+        res.status(200).send('user authentificated...')
+
+    }
+    
+})
+
+
+
+//export the module router
 module.exports =router;
