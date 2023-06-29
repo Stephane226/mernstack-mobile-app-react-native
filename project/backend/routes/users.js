58bcd027b7d5a7d1faeafc30be49222d0a98eee9
Masaaki Stephane <62558026+Stephane226@users.noreply.github.com>
2023-06-29 10:09:43 +0300

    new user and user model

--- project/backend/routes/users.js
+++ project/backend/routes/users.js
@@ -11,4 +11,30 @@ router.get(`/`, async (req, res) =>{
     res.send(userList);
 })
 
+
+//new user regis
+router.post('/' , async (req,res) =>{
+    let user = new User({
+        name: req.body.name,
+        email: req.body.email,
+        passwordHash: req.body.passwordHash,
+        phone: req.body.phone,
+        isAdmin: req.body.isAdmin,
+        street: req.body.street,
+        apartment: req.body.apartment,
+        zip: req.body.zip,
+        city: req.body.city,
+        country: req.body.country,
+    })
+
+    user = await user.save();
+
+    if(!user){
+        return res.status(404).send('the user cannot be created...')
+    }
+
+    res.send(user);
+
+})
+
 module.exports =router;
