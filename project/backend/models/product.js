6d12ccdd8b2636ab2eeb7b6bdb31c7ae94f047e1
Masaaki Stephane <62558026+Stephane226@users.noreply.github.com>
2023-07-04 23:43:34 +0300

    formats

--- project/backend/models/product.js
+++ project/backend/models/product.js
@@ -1,71 +1,71 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const productSchema = mongoose.Schema({
-    name: {
+  name: {
+    type: String,
+    required: true,
+  },
+
+  description: {
+    type: String,
+    required: "enter descr",
+  },
+
+  richDescription: {
+    type: String,
+    dafault: "",
+  },
+  image: {
+    type: String,
+    dafault: "",
+  },
+  images: {
+    type: String,
+    dafault: [
+      {
         type: String,
-        required : true,
-    },
-    
-    description: {
-        type: String,
-        required : 'enter descr',
-    },
+      },
+    ],
+  },
+  brand: {
+    type: String,
+    dafault: "",
+  },
 
-    richDescription: {
-        type: String,
-        dafault : '',
-    },
-    image: {
-        type: String,
-        dafault : '',
-    },
-    images: {
-        type: String,
-        dafault : [{
-            type: String
-        }],
-    },
-    brand: {
-        type: String,
-        dafault : '',
-    },
+  price: {
+    type: Number,
+    default: 0,
+  },
+  category: {
+    type: mongoose.Schema.Types.ObjectId,
+    dafault: 0,
+    ref: "Category",
+  },
 
-    price: {
-        type: Number,
-        default : 0,
-    },
-    category: {
-        type: mongoose.Schema.Types.ObjectId,
-        dafault : 0,
-        ref : 'Category'
-    },
-  
-    countInStock: {
-        type: Number,
-        required: true,
-        min:0,
-        max: 255
-    },
-    rating: {
-        type: Number,
-        default : 0,
-    },
-    numReviews: {
-        type: Number,
-        default : 0,
-    },
+  countInStock: {
+    type: Number,
+    required: true,
+    min: 0,
+    max: 255,
+  },
+  rating: {
+    type: Number,
+    default: 0,
+  },
+  numReviews: {
+    type: Number,
+    default: 0,
+  },
 
-    isFeatured: {
-        type: Boolean,
-        default : false,
-    },
+  isFeatured: {
+    type: Boolean,
+    default: false,
+  },
 
-    dateCreated: {
-        type: Date,
-        default : Date.now,
-    },
+  dateCreated: {
+    type: Date,
+    default: Date.now,
+  },
+});
 
-
-})
-
-exports.Product = mongoose.model('Product', productSchema);
+exports.Product = mongoose.model("Product", productSchema);
--- project/backend/routes/users.js
+++ project/backend/routes/users.js
@@ -1,101 +1,97 @@
-const {User} = require('../models/user');
-const express = require('express');
+const { User } = require("../models/user");
+const express = require("express");
 const router = express.Router();
-const bcrypt = require('bcrypt')
-const jwt = require('jsonwebtoken')
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
-router.get(`/`, async (req, res) =>{
-    const userList = await User.find();
+router.get(`/`, async (req, res) => {
+  const userList = await User.find();
 
-    if(!userList) {
-        res.status(500).json({success: false})
-    } 
-    res.send(userList);
-})
-
+  if (!userList) {
+    res.status(500).json({ success: false });
+  }
+  res.send(userList);
+});
 
 //new user regis
-router.post('/' , async (req,res) =>{
-    let user = new User({
-        name: req.body.name,
-        email: req.body.email,
-        passwordHash: bcrypt.hashSync(req.body.password, 10),
-        phone: req.body.phone,
-        isAdmin: req.body.isAdmin,
-        street: req.body.street,
-        apartment: req.body.apartment,
-        zip: req.body.zip,
-        city: req.body.city,
-        country: req.body.country,
-    })
+router.post("/", async (req, res) => {
+  let user = new User({
+    name: req.body.name,
+    email: req.body.email,
+    passwordHash: bcrypt.hashSync(req.body.password, 10),
+    phone: req.body.phone,
+    isAdmin: req.body.isAdmin,
+    street: req.body.street,
+    apartment: req.body.apartment,
+    zip: req.body.zip,
+    city: req.body.city,
+    country: req.body.country,
+  });
 
-    user = await user.save();
+  user = await user.save();
 
-    if(!user){
-        return res.status(404).send('the user cannot be created...')
-    }
+  if (!user) {
+    return res.status(404).send("the user cannot be created...");
+  }
 
-    res.send(user);
-
-})
-
-
+  res.send(user);
+});
 
 //get list of users
 
-router.get(`/`, async (req, res) =>{
-    const users = await User.find().select("-passwordHash");
+router.get(`/`, async (req, res) => {
+  const users = await User.find().select("-passwordHash");
 
-    if(!users) {
-        res.status(500).json({success: false, message : "no user found"})
-    } 
-    res.send(users);
-})
-
+  if (!users) {
+    res.status(500).json({ success: false, message: "no user found" });
+  }
+  res.send(users);
+});
 
 //find a user by id
 
-router.get('/:id', async  (req, res) => {
-    const user = await User.findById(req.params.id).select("name phone email");
+router.get("/:id", async (req, res) => {
+  const user = await User.findById(req.params.id).select("name phone email");
 
-    if(!user){
-        return res.status(404).send('the category cannot be created...')
-    }
+  if (!user) {
+    return res.status(404).send("the category cannot be created...");
+  }
 
-    res.send(user);
-    
-})
-
+  res.send(user);
+});
 
 //user login checks
 
-router.post('/login', async  (req, res) => {
-    const user = await User.findOne({email : req.body.email})
-    const secret = process.env.secret;
+router.post("/login", async (req, res) => {
+  const user = await User.findOne({ email: req.body.email });
+  const secret = process.env.secret;
 
-    if(!user){
-        return res.status(404).send('user not exist...')
-    }
+  if (!user) {
+    return res.status(404).send("user not exist...");
+  }
 
-    if(user && bcrypt.compareSync( req.body.password , user.passwordHash)){
-        const token  = jwt.sign({
-            userID : user.id
-
-        },
-        secret,
-        {
-            expiresIn : '1d'
-        }
-        )
-        res.status(200).send({message :'user authentificated...', token : token, user : user.email, secret : secret})
-
-    }else{
-        return res.send('password is wrong')
-    }
-    
-})
-
-
+  if (user && bcrypt.compareSync(req.body.password, user.passwordHash)) {
+    const token = jwt.sign(
+      {
+        userID: user.id,
+      },
+      secret,
+      {
+        expiresIn: "1d",
+      }
+    );
+    res
+      .status(200)
+      .send({
+        message: "user authentificated...",
+        token: token,
+        user: user.email,
+        secret: secret,
+      });
+  } else {
+    return res.send("password is wrong");
+  }
+});
 
 //export the module router
-module.exports =router;
+module.exports = router;
