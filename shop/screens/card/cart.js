3bfa6e58f2fca4fdbcb4615c0998b627728545c3
Masaaki Stephane <62558026+Stephane226@users.noreply.github.com>
2023-07-13 22:56:19 +0300

    cart stylings

--- shop/screens/card/cart.js
+++ shop/screens/card/cart.js
@@ -18,10 +18,10 @@ const Cart = (props) => {
         <View style={styles.container}>
           {props.cartItems.map((x) => {
             return (
-              <View style={{flex:1,width:'100%'}}>
-                 <Image style={{width:60, height:60}}
+              <View style={{flex:1,width:'100%',backgroundColor:'white',flexDirection:'row',justifyContent:'space-around',alignItems:'center',marginVertical:5,paddingVertical:10,}}>
+                 <Image style={{width:60, height:60,borderRadius:'50%'}}
                 resizeMode="cover"
-                source={{uri : x.product.name?  x.product.name : 'https://cdn.pixabay.com/photo/2012/04/01/17/29/box-23649_960_720.png'}}
+                source={{uri : x.product.image?  x.product.image : 'https://cdn.pixabay.com/photo/2012/04/01/17/29/box-23649_960_720.png'}}
                 />
                  <Text>{x.product.name}</Text>
                  <Text>{x.product.price}</Text>
@@ -48,7 +48,7 @@ const mapStateToProps = (state) => {
 const styles = StyleSheet.create({
   container: {
     width: "100%",
-    height:300,
+    height:500,
     flexDirection: 'column',
     alignContent: "center",
     justifyContent: "center",
