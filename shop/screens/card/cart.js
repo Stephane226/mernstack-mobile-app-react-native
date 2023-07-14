85128e338e6d2c421f5c72a0e18c0671d774e3e7
Masaaki Stephane <62558026+Stephane226@users.noreply.github.com>
2023-07-14 16:44:16 +0300

    cart page remove items funct

--- shop/screens/card/cart.js
+++ shop/screens/card/cart.js
@@ -1,4 +1,5 @@
-import React from "react";
+import React ,{ useEffect, 
+  useState }from "react";
 import {
   StyleSheet,
   View,
@@ -6,29 +7,71 @@ import {
   Image,
   Text,
   SafeAreaView,
+  Button,
+ 
 } from "react-native";
-
+import * as actions from '../../Redux/actions/cartActions'
 import { connect } from "react-redux";
+import {SwipelistView} from 'react-native-swipe-list-view'
+const data = require("../../assets/data/products.json");
 
 const Cart = (props) => {
+
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    setProducts(data);
+  }, [])
+  var total = 0
+  props.cartItems.forEach(element => {
+    total += element.product.price
+  });
   return (
     <>
    
       {props.cartItems.length ? (
+          <View style={styles.container1}> 
         <View style={styles.container}>
           {props.cartItems.map((x) => {
             return (
-              <View style={{flex:1,width:'100%',backgroundColor:'white',flexDirection:'row',justifyContent:'space-around',alignItems:'center',marginVertical:5,paddingVertical:10,}}>
+              <View style={{flex:1,width:'100%',backgroundColor:'white',flexDirection:'row',justifyContent:'space-between',alignItems:'center',marginVertical:5,paddingVertical:10,}}>
                  <Image style={{width:60, height:60,borderRadius:'50%'}}
                 resizeMode="cover"
                 source={{uri : x.product.image?  x.product.image : 'https://cdn.pixabay.com/photo/2012/04/01/17/29/box-23649_960_720.png'}}
                 />
                  <Text>{x.product.name}</Text>
                  <Text>{x.product.price}</Text>
+               <Button
+                   title='Remove'
+                  onPress={ () => {
+                    const itemToRemove = () => {
+                      return products.filter(item => item._id.$oid == x.product._id.$oid)
+                      }
+                     // console.log(itemToRemove())
+                    props.removeFromCart(itemToRemove()[0])
+                   // console.log(products.filter(item => item._id.$oid == x.product._id.$oid))
+                   console.log(itemToRemove()[0]._id.$oid)
+                
+                  }}
+
+                 /> 
               </View>
+              
             )
           })}
+        </View> 
+        <View style={{flex:1,width:'90%',    marginHorizontal:'5%',backgroundColor:'white',flexDirection:'row',justifyContent:'space-around',alignItems:'center',marginVertical:5,paddingVertical:5}}>
+              <Text style={{color:'red'}}> $ {total}</Text>
+                 <Button
+                   title='Clear'
+                   onPress={ () =>{
+                    props.clearCart()
+                   }}
+                 />
+                   <Text> Checkout</Text>
         </View>
+        </View>
+
       ) : (
         <View>
             <Text>cart is empty...</Text>
@@ -45,16 +88,34 @@ const mapStateToProps = (state) => {
   };
 };
 
+const mapDispatchToProps = (dispatch) =>{
+  return{
+    clearCart : () => dispatch(actions.clearCart()),
+    removeFromCart : (item) => dispatch(actions.removeFromCart(item))
+  }
+}
 const styles = StyleSheet.create({
-  container: {
+  container1: {
     width: "100%",
-    height:500,
+    height:'100%',
+    flexDirection: 'column',
+    alignContent: "center",
+    justifyContent: "center",
+   
+  },
+    container: {
+    width: "100%",
+    height:'90%',
     flexDirection: 'column',
     alignContent: "center",
     justifyContent: "center",
     padding: 20,
     marginTop: 0,
+
   },
+  containerTotal:{
+
+  }
 });
 
-export default connect(mapStateToProps, null)(Cart);
+export default connect(mapStateToProps, mapDispatchToProps)(Cart);
