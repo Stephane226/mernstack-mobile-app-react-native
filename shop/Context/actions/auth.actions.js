2eb6fa03550f43efcf1563e9de3dc34089403ada
Masaaki Stephane <62558026+Stephane226@users.noreply.github.com>
2023-07-16 10:47:52 +0300

    reducers

--- shop/Context/actions/auth.actions.js
+++ shop/Context/actions/auth.actions.js
@@ -2,6 +2,7 @@ import jwt_decode from 'jwt-decode'
 import AsyncStorage from '@react-native-community/async-storage'
 import Toast from 'react-native-toast-message'
 import baseUrl from '../../assets/common/baseUrl'
+import { decode } from 'punycode'
 
 
 export const SET_CURRENT_USER = 'SET_CURRENT_USER'
@@ -21,6 +22,47 @@ export const loginUser = (user, dispatch) => {
         if(data){
             const token = data.token;
             AsyncStorage.setItem('jwt', token)
+            AsyncStorage.setItem('message2', 'you are logged')
+            const decoded = jwt_decode(token)
+            dispatch(setCurentUser(decoded , user)) 
+
+        }else{
+           logoutUser(dispatch)
         }
+     }).catch((err) =>{
+        Toast.show({
+            topOffset : 60,
+            type : 'error',
+            text1 :'please provide correct credentials',
+            text2 : ''
+        })
      })
+}
+
+export const getUserProfile = (id) =>{
+   fetch(`${baseUrl}users/${id}` , {
+    method: 'GET',
+    body : JSON.stringify(id),
+    header :{
+        Accept : 'application/json',
+        'Content-Type' : 'application/json',
+    }
+   })
+   .then((res) => res.json())
+   .then((data) => console.log(data)) 
+
+}
+
+
+export const logoutUser = (dispatch) =>{
+    AsyncStorage.removeItem('jwt');
+    dispatch(setCurentUser({}))
+}
+
+export const  setCurentUser = (decoded, user) =>{
+    return{
+         type : SET_CURRENT_USER,
+         payload : decoded,
+         userProfile : user
+    }
 }
